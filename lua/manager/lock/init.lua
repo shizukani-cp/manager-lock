a0535bab4f20889b0c1d9117034e3ffa4a4490f8
shizukani-cp <shizukani.cp@gmail.com>
2026-02-22 09:37:22 +0900

    chore: add StyLua configuration file

--- lua/manager/lock/init.lua
+++ lua/manager/lock/init.lua
@@ -6,24 +6,24 @@ M.load_queue = {}
 local manager = require("manager.core")
 
 function M.lock()
-	M.is_locked = true
+    M.is_locked = true
 end
 
 function M.unlock()
-	M.is_locked = false
-	local queue = M.load_queue
-	M.load_queue = {}
-	for _, id in ipairs(queue) do
-		manager.load(id)
-	end
+    M.is_locked = false
+    local queue = M.load_queue
+    M.load_queue = {}
+    for _, id in ipairs(queue) do
+        manager.load(id)
+    end
 end
 
 function M.locked_load(id)
-	if M.is_locked then
-		table.insert(M.load_queue, id)
-		return
-	end
-	manager.load(id)
+    if M.is_locked then
+        table.insert(M.load_queue, id)
+        return
+    end
+    manager.load(id)
 end
 
 M.load = M.locked_load
