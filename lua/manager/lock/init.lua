ee7fa9f9dcd417f2b822d300b8f63dfb079d0de8
shizukani-cp <shizukani.cp@gmail.com>
2026-03-01 16:26:10 +0900

    chore: add type definitions

--- lua/manager/lock/init.lua
+++ lua/manager/lock/init.lua
@@ -1,9 +1,19 @@
+---@class Manager
+---@field is_locked boolean
+---@field load_queue string[]
+---@field lock fun(self: Manager)
+---@field unlock fun(self: Manager)
+---@field original_load fun(self: Manager, id: string)
+---@field locked_load fun(self: Manager, id: string)
+
 local M = {}
 
+---@param self Manager
 local function lock(self)
     self.is_locked = true
 end
 
+---@param self Manager
 local function unlock(self)
     self.is_locked = false
     local queue = self.load_queue
@@ -13,6 +23,8 @@ local function unlock(self)
     end
 end
 
+---@param self Manager
+---@param id string
 local function locked_load(self, id)
     if self.is_locked then
         table.insert(self.load_queue, id)
@@ -21,6 +33,8 @@ local function locked_load(self, id)
     self:original_load(id)
 end
 
+---@param manager Manager
+---@param override? boolean
 function M.setup(manager, override)
     manager.lock = lock
     manager.unlock = unlock
